b750b771b39fd610637ca396c7bbca96e40c276f
Sonic Lucas Maurice <sonic@djls.io>
2024-01-30 02:26:22 -0500

    WIP...

--- satisfactory-metadata/main.go
+++ satisfactory-metadata/main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"net/http"
 
+	"github.com/benbjohnson/clock"
 	_ "github.com/lib/pq"
 )
 
 // Define parameters
 var (
-	logLevel      = flag.String("log.level", "info", "Only log messages with the given severity or above. One of: [debug, info, warn, error, none]")
 	frmApiAddress = flag.String("frm.listen-address", "http://localhost:8080", "Address of Ficsit Remote Monitoring webserver")
 
 	pgHost     = flag.String("db.pghost", "postgres", "postgres hostname")
@@ -18,6 +20,9 @@ var (
 	pgPassword = flag.String("db.pgpassword", "secretpassword", "postgres password")
 	pgUser     = flag.String("db.pguser", "postgres", "postgres username")
 	pgDb       = flag.String("db.pgdb", "postgres", "postgres db")
+
+	Clock = clock.New()
+	now   = Clock.Now()
 )
 
 func main() {
@@ -27,39 +32,180 @@ func main() {
 	// Generate connection string
 	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", *pgHost, *pgPort, *pgUser, *pgPassword, *pgDb)
 
-	// Print connection string for debugging purposes
-	fmt.Println(psqlconn)
-
 	// Connect to database
 	db, err := sql.Open("postgres", psqlconn)
-
 	if err != nil {
 		panic("Failed to connect to database: " + err.Error())
 	}
 	defer db.Close()
 
+	// Ping database to ensure connection is still alive
 	err = db.Ping()
-
 	if err != nil {
 		panic("Failed to ping database: " + err.Error())
 	}
 
-	// 	cacheWorker := NewCacheWorker("http://"+frmHostname+":"+strconv.Itoa(frmPort), db)
-	// 	go cacheWorker.Start()
+	// Initialize database
+	err = initDB(db)
+	if err != nil {
+		panic("Failed to initialize database: " + err.Error())
+	}
 
-	// 	fmt.Printf(`
-	// FRM Cache started
-	// Press ctrl-c to exit`)
+	err = flushMetricHistory(db)
+	if err != nil {
+		panic("Failed to flush metric history: " + err.Error())
+	}
 
-	// 	// Wait for an interrupt signal
-	// 	sigChan := make(chan os.Signal, 1)
-	// 	if runtime.GOOS == "windows" {
-	// 		signal.Notify(sigChan, os.Interrupt)
-	// 	} else {
-	// 		signal.Notify(sigChan, syscall.SIGTERM)
-	// 		signal.Notify(sigChan, syscall.SIGINT)
-	// 	}
-	// 	<-sigChan
+	// Low cadence metrics
+	pullMetrics(db, "factory", "/getFactory", true)
+	pullMetrics(db, "extractor", "/getExtractor", true)
+	pullMetrics(db, "dropPod", "/getDropPod", false)
+	pullMetrics(db, "storageInv", "/getStorageInv", false)
+	pullMetrics(db, "worldInv", "/getWorldInv", false)
+	pullMetrics(db, "droneStation", "/getDroneStation", false)
 
-	// 	cacheWorker.Stop()
+	// Realtime metrics
+	pullMetrics(db, "drone", "/getDrone", true)
+	pullMetrics(db, "train", "/getTrains", true)
+	pullMetrics(db, "truck", "/getVehicles", true)
+	pullMetrics(db, "trainStation", "/getTrainStation", true)
+	pullMetrics(db, "truckStation", "/getTruckStation", true)
+}
+
+// flush the metric history cache
+func initDB(db *sql.DB) error {
+	req := `CREATE TABLE IF NOT EXISTS cache(
+		id serial primary key,
+		metric text NOT NULL,
+		frm_data jsonb
+	  );
+	  CREATE INDEX cache_metric ON cache(metric);
+	  
+	  CREATE TABLE IF NOT EXISTS cache_with_history(
+		id serial primary key,
+		metric text NOT NULL,
+		frm_data jsonb,
+		time timestamp
+	  );`
+
+	_, err := db.Exec(req)
+	if err != nil {
+		fmt.Println("flush metrics history db error: ", err)
+	}
+	return err
+}
+
+// flush the metric history cache
+func flushMetricHistory(db *sql.DB) error {
+	delete := `truncate cache_with_history;`
+	_, err := db.Exec(delete)
+	if err != nil {
+		fmt.Println("flush metrics history db error: ", err)
+	}
+	return err
+}
+
+// pull metrics from the Ficsit Remote Monitoring API
+func pullMetrics(db *sql.DB, metric string, route string, keepHistory bool) {
+	resp, err := http.Get(*frmApiAddress + route)
+
+	if err != nil {
+		fmt.Println("error when parsing json: %s", err)
+		return
+	}
+
+	var content []json.RawMessage
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&content)
+	if err != nil {
+		fmt.Println("error when parsing json: %s", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	data := []string{}
+	for _, c := range content {
+		data = append(data, string(c[:]))
+	}
+
+	err = cacheMetrics(db, metric, data)
+
+	if err != nil {
+		fmt.Println("error when caching metrics %s", err)
+		return
+	}
+
+	if keepHistory {
+		err = cacheMetricsWithHistory(db, metric, data)
+		if err != nil {
+			fmt.Println("error when caching metrics history %s", err)
+			return
+		}
+	}
+}
+
+// cache metrics in the database
+func cacheMetrics(db *sql.DB, metric string, data []string) (err error) {
+	tx, err := db.Begin()
+	if err != nil {
+		return
+	}
+	defer func() {
+		switch err {
+		case nil:
+			err = tx.Commit()
+		default:
+			tx.Rollback()
+		}
+	}()
+
+	delete := `delete from cache where metric = $1;`
+	_, err = tx.Exec(delete, metric)
+	if err != nil {
+		return
+	}
+	for _, s := range data {
+		insert := `insert into cache (metric,data) values($1,$2)`
+		_, err = tx.Exec(insert, metric, s)
+		if err != nil {
+			return
+		}
+	}
+	return
+}
+
+// cache metrics in the database with history
+func cacheMetricsWithHistory(db *sql.DB, metric string, data []string) (err error) {
+	tx, err := db.Begin()
+	if err != nil {
+		return
+	}
+	defer func() {
+		switch err {
+		case nil:
+			err = tx.Commit()
+		default:
+			tx.Rollback()
+		}
+	}()
+	for _, s := range data {
+		insert := `insert into cache_with_history (metric,data, time) values($1,$2,$3)`
+		_, err = tx.Exec(insert, metric, s, now)
+		if err != nil {
+			return
+		}
+	}
+
+	//720 = 1 hour, 5 second increments. retain that many rows for every data.
+	keep := 720 * len(data)
+
+	delete := `delete from cache_with_history where
+metric = $1 and
+id NOT IN (
+select id from "cache_with_history" where metric = $1
+order by id desc
+limit $2
+);`
+	_, err = tx.Exec(delete, metric, keep)
+	return
 }
